56050401e39412611863d0de37baf85ab4223bb3
Ben <greatmaharg@gmail.com>
2022-11-15 23:07:39 -0500

    New edgegate logic

--- Simulation/src/edgegate.v
+++ Simulation/src/edgegate.v
@@ -6,24 +6,42 @@ module edgegate(
         output clkout
     );
 
-    // RS latch
-    wire nclkup;
-    nand00 engate(
-        .a(clk),
-        .b(en),
-        .q(nclkup)
-    );
+    wire dl_q;
+    wire dl_nq;
+    wire ndl_q;
+    wire ndl_nq;
+    wire nq;
 
-    wire nclkout;
-    nand00 latch_q(
-        .a(nclkup),
-        .b(nclkout),
+    nand00 g_din(
+        .a(dl_q),
+        .b(en),
+        .q(dl_nq)
+    );
+    nand10 g_tri(
+        .a(ndl_nq),
+        .b(clk),
+        .c(dl_nq),
+        .q(dl_q)
+    );
+    nand00 g_nd(
+        .a(dl_nq),
+        .b(ndl_nq),
+        .q(ndl_q)
+    );
+    nand00 g_ndn(
+        .a(ndl_q),
+        .b(clk),
+        .q(ndl_nq)
+    );
+    nand00 g_q(
+        .a(ndl_nq),
+        .b(nq),
         .q(clkout)
     );
-    nand00 latch_nq(
-        .a(clk),
-        .b(clkout),
-        .q(nclkout)
+    nand00 g_nq(
+        .a(clkout),
+        .b(clk),
+        .q(nq)
     );
 
 endmodule
