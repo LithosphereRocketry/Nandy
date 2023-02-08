1f0bbdb6537ab44866bf3576b5d7f31bc7c5dd58
Ben <greatmaharg@gmail.com>
2023-02-07 21:02:16 -0500

    Start on reworking emulator

--- Toolchain/src/bin_emulator.c
+++ Toolchain/src/bin_emulator.c
@@ -4,7 +4,47 @@
 #include <string.h>
 
 typedef int8_t word_t;
+typedef uint16_t addr_t;
 typedef uint8_t inst_t;
+
+#define MULTICYCLE_MASK 1<<7
+#define ALU_SEL_MASK 1<<6
+#define ISP_MASK 1<<5
+#define XY_MASK 1<<5
+#define JUMP_MASK 1<<5
+#define CARRY_SEL_MASK 1<<4
+#define PROGFLOW_MASK 1<<4
+#define JUMP_COND_MASK 1<<4
+#define BRK_MASK 1<<3
+#define WR_MASK 1<<3
+#define RD_MASK 1<<2
+
+#define ALU_INST_MASK 0xF
+#define ALU_WRITESBOTH_MASK 0b1100 // any instruction with either of these bits can
+								   // write ACC and CARRY simultaneously
+#define REGID_MASK 0b0011
+
+/*
+*/
+
+enum ALUMode {
+	ALU_B = 0x0,
+	ALU_XOR = 0x1,
+	ALU_AND = 0x2,
+	ALU_OR = 0x3,
+	ALU_ADD = 0x4,
+	ALU_ADDC = 0x5,
+	ALU_SUB = 0x6,
+	ALU_SUBC = 0x7,
+	ALU_SL = 0x8,
+	ALU_SLC = 0x9,
+	ALU_SLA = 0xA,
+	ALU_SLR = 0xB,
+	ALU_SR = 0xC,
+	ALU_SRC = 0xD,
+	ALU_SRR = 0xE,
+	ALU_SRA = 0xF
+};
 
 enum {
 	RADIX_DECIMAL,
@@ -12,36 +52,37 @@ enum {
 	RADIX_HEX
 } radix = RADIX_HEX;
 
-unsigned long cycles = -1;
-word_t acc, data, sp;
-bool carry;
-word_t pc = -1;
-word_t ram[256];
-const inst_t rom[256] = {
-	0b11001110, // 	brk
-	0b01010100, // 	lui 10
-	0b00000100, // 	lui 0
-	0b11000001, // 	swd
-	0b00000110, // 	andi 0
-	// loop:
-	0b11110000, // 	add
-	0b11000001, // 	swd
-	0b00001011, // 	subi 1
-	0b11000001, // 	swd
-	0b11001011, // 	jnif ...
-	5, // loop
-	0b11000100, // 	ioa
-	0b11001110  // 	brk
+enum RegID {
+	REGID_SP = 0x0,
+	REGID_IO = 0x1,
+	REGID_DX = 0x2,
+	REGID_DY = 0x3
 };
-word_t ioain, iobin;
-word_t ioaout, iobout;
+
+unsigned long cycles = -1;
+word_t acc, x, y, sp;
+bool carry;
+addr_t pc = -1;
+inst_t mem[65536] = {
+	0x1B, // brk
+	0xC0, // rdi
+	10,
+	0x0A, // wr x
+	0xC0, // rdi
+	20,
+	0x44, // _add x
+	0xEF, // j
+	-2
+};
+word_t* ram = mem + 32768;
+word_t ioin, ioout;
 
 word_t signExt(word_t value, int bits) {
 	word_t mask = (-1) << bits;
 	if(value & (1 << (bits-1))) {
 		return value | mask;
 	} else {
-		return value;
+		return value & ~mask;
 	}
 }
 
@@ -52,17 +93,17 @@ bool pause() {
 	while(1) {
 		switch(radix) {
 			case RADIX_DECIMAL:
-				printf("    PC %hhi\tCycles %hhi\t   ACC %hhi\t IOAIN %hhi\t IOBIN %hhi\n  Inst %hhi\t    SP %hhi\t  DATA %hhi\tIOAOUT %hhi\tIOBOUT %hhi\n> ",
-					pc, cycles, acc, ioain, iobin, rom[pc], sp, data, ioaout, iobout);
+				printf("    PC %hhi\t   ACC %hhi\t  IOIN %hhi\tCycles %hhi\n  Inst %hhi\t    SP %hhi\t    DX %hhi\t    DY %hhi\tIOOUT %hhi\n> ",
+					pc, acc, ioin, cycles, mem[pc], sp, x, ioout);
 				break;
 			case RADIX_UNSIGNED:
-				printf("    PC %hhu\tCycles %hhu\t   ACC %hhu\t IOAIN %hhu\t IOBIN %hhu\n  Inst %hhu\t    SP %hhu\t  DATA %hhu\tIOAOUT %hhu\tIOBOUT %hhu\n> ",
-					pc, cycles, acc, ioain, iobin, rom[pc], sp, data, ioaout, iobout);
+				printf("    PC %hhu\t   ACC %hhu\t  IOIN %hhu\tCycles %hhu\n  Inst %hhu\t    SP %hhu\t    DX %hhu\t    DY %hhu\tIOOUT %hhu\n> ",
+					pc, acc, ioin, cycles, mem[pc], sp, x, ioout);
 				break;
 			case RADIX_HEX:
 			default:
-				printf("    PC 0x%hhx\tCycles 0x%hhx\t   ACC 0x%hhx\t IOAIN 0x%hhx\t IOBIN 0x%hhx\n  Inst 0x%hhx\t    SP 0x%hhx\t  DATA 0x%hhx\tIOAOUT 0x%hhx\tIOBOUT 0x%hhx\n> ",
-					pc, cycles, acc, ioain, iobin, rom[pc], sp, data, ioaout, iobout);
+				printf("    PC 0x%hhx\t   ACC 0x%hhx\t  IOIN 0x%hhx\tCycles 0x%hhx\n  Inst 0x%hhx\t    SP 0x%hhx\t    DX 0x%hhx\t    DY 0x%hhx\tIOOUT 0x%hhx\n> ",
+					pc, acc, ioin, cycles, mem[pc], sp, x, ioout);
 		}
 		char input[256];
 		fgets(input, 255, stdin);
@@ -95,236 +136,95 @@ bool pause() {
 
 inst_t fetch() {
 	cycles++;
-	return rom[++pc];
+	return mem[++pc];
+}
+
+void aluop(enum ALUMode mode, bool isCarry, word_t a, word_t b, word_t* result) {
+	bool newcarry = false;
+	int newresult = 0;
+	switch(mode) {
+		case ALU_B:
+			newresult = b;
+			newcarry = (a != b);
+			break;
+		case ALU_ADD:
+			newresult = a + b;
+			newcarry = (newresult >> 8) & 1;
+			break;
+		default:
+			printf("ALU operation 0x%x not implemented\n", mode);
+	}
+	if(isCarry) {
+		carry = newcarry;
+		if(mode & ALU_WRITESBOTH_MASK) {
+			*result = newresult;
+		}
+	} else {
+		*result = newresult;
+	}
 }
 
 bool step(bool interrupt) {
-	inst_t op = fetch();
-	if((op & 0b10000000) == 0) { // no first bit = 4 bit imm
-		word_t imm = (op & 0b01111000) >> 3;
-		word_t result;
-		switch(op & 0b111) {
-			case 0: // isp
-				result = ((int) sp) + imm;
-				sp = result;
-				carry = (result >> 8) & 1;
-				break;
-			case 1: // addi
-				result = ((int) acc) - imm;
-				acc = result;
-				carry = (result >> 8) & 1;
-				break;
-			case 2: // dsp
-				result = ((int) sp) - imm;
-				sp = result;
-				carry = (result >> 8) & 1;
-				break;
-			case 3: // subi
-				result = ((int) acc) - imm;
-				acc = result;
-				carry = (result >> 8) & 1;
-				break;
-			case 4: // lui
-				carry = acc != signExt(imm, 4);
-				acc = ((acc >> 4) & 0xF) | imm << 4;
-				break;
-			case 5: // xori
-				carry = acc == signExt(imm, 4);
-				acc ^= signExt(imm, 4);
-				break;
-			case 6: // andi
-				carry = acc < signExt(imm, 4);
-				acc &= signExt(imm, 4);
-				break;
-			case 7: // ori
-				carry = acc >= signExt(imm, 4);
-				acc |= signExt(imm, 4);
-				break;
-		}
-	} else if((op & 0b01000000) == 0) { // no second bit = 3 bit imm
-		int8_t imm = (op & 0b00111000) >> 3;
-		switch(op & 0b111) {
-			case 0: // stra
-				ram[data + imm] = acc;
-				break;
-			case 1: // strs
-				ram[sp + imm] = acc;
-				break;
-			case 2: // stran
-				ram[data - imm] = acc;
-				break;
-			case 3: // strsn
-				ram[sp - imm] = acc;
-				break;
-			case 4: // lda
-				acc = ram[data + imm];
-				break;
-			case 5: // lds
-				acc = ram[sp + imm];
-				break;
-			case 6: // ldan
-				acc = ram[data - imm];
-				break;
-			case 7: // ldsn
-				acc = ram[sp - imm];
-				break;
-		}
-	} else if((op & 0b00110000) == 0) { // no third and fourth bits = special instruction
-		if((op & 0b00001000) == 0) { // no fifth bit = register transfer
-			word_t temp;
-			switch(op & 0b111) {
-				case 0: // wrd
-					data = acc;
-					break;
-				case 1: // swd
-					temp = acc;
-					acc = data;
-					data = temp;
-					break;
-				case 2: // wrs
-					sp = acc;
-					break;
-				case 3: // sws
-					temp = acc;
-					acc = sp;
-					sp = temp;
-					break;
-				case 4: // ioa
-					ioaout = acc;
-					acc = ioain;
-					break;
-				case 5: // rdd
-					acc = data;
-					break;
-				case 6: // iob
-					iobout = acc;
-					acc = iobin;
-					break;
-				case 7: // rds
-					acc = sp;
-					break;
-			}
-		} else { // has fifth bit = jumps and weirdness
-			if((op & 0b00000100) == 0) { // no sixth bit = multicycle
-				inst_t datafield = fetch();
-				switch(op & 0b11) {
-					case 0: // j
-						pc = datafield-1;
-						break;
-					case 1: // jsr
-						data = pc+1;
-						pc = datafield-1;
-						break;
-					case 2: // jif
-						if(carry) {
-							pc = datafield-1;
-						}
-						break;
-					case 3: // jnif
-						if(!carry) {
-							pc = datafield-1;
-						}
-						break;
+	inst_t i = fetch();
+	if(!(i & MULTICYCLE_MASK)) { // single cycle operations
+		if(!(i & ALU_SEL_MASK)) { // basic instructions
+			if(!(i & ISP_MASK)) { // housekeeping/program logic
+				if(!(i & PROGFLOW_MASK)) { // Register swaps
+					word_t from;
+					word_t* to;
+					switch(i & REGID_MASK) {
+						case REGID_SP:
+							from = sp;
+							to = &sp;
+							break;
+						case REGID_IO:
+							from = ioin;
+							to = &ioout;
+							break;
+						case REGID_DX:
+							from = x;
+							to = &x;
+							break;
+						case REGID_DY:
+							from = y;
+							to = &y;
+							break;
+					}
+					if(i & WR_MASK) {
+						*to = acc;
+					}
+					if(i & RD_MASK) {
+						acc = from;
+					}
+				} else { // prorgam flow
+					if(!(i & BRK_MASK)) { // ret/call
+
+					} else { // break
+						if(interrupt) return pause();
+					}
 				}
-			} else { // single cycle program flow
-				switch(op & 0b11) {
-					case 0: // ret
-						pc = data;
-						break;
-					case 1: // retsr
-						word_t temp = pc+1;
-						pc = data-1;
-						data = temp;
-						break;
-					case 2: // brk
-						if(interrupt) {
-							return pause();
-						}
-						break;
-					case 3: // nop
-						break;
+			} else { // isp instructions
+				aluop(ALU_ADD, i & CARRY_SEL_MASK, sp, signExt(i, 4), &sp);
+			}
+		} else { // normal math
+			word_t alub = (i & XY_MASK) ? y : x;
+			aluop(i & ALU_INST_MASK, i & CARRY_SEL_MASK, acc, alub, &acc);
+		}
+	} else { // multicycle operations
+		word_t imm = fetch();
+		if(!(i & ALU_SEL_MASK)) { // memory 
+		} else { // immediates
+			if(!(i & JUMP_MASK)) { // immediate ops
+				aluop(i & ALU_INST_MASK, i & CARRY_SEL_MASK, acc, imm, &acc);
+			} else { // jumps
+				if(!(i & JUMP_COND_MASK)) { // unconditional jumps
+					int offs = (((int) signExt(i, 4)) << 8) + ((int) imm) - 1;
+					printf("%i %i %i\n", signExt(i, 4), imm, offs);
+					pc += offs;
+				} else {
+					// conditional jumps
 				}
 			}
-		}
-	} else { // ALU operations
-		bool carryRes;
-		word_t result;
-		int longres;
-		switch(op & 0b00001111) {
-			case 0: // add
-				longres = acc + data;
-				result = longres & 0xFF;
-				carryRes = (longres >> 8) & 1;
-				break;
-			case 1: // addc
-				longres = acc + data + carry;
-				result = longres & 0xFF;
-				carryRes = (longres >> 8) & 1;
-				break;
-			case 2: // sub
-				longres = acc + ~data + 1; // this is the same as acc - data
-				result = longres & 0xFF;
-				carryRes = (longres >> 8) & 1;
-				break;
-			case 3: // subc
-				longres = acc + ~data + carry;
-				result = longres & 0xFF;
-				carryRes = (longres >> 8) & 1;
-				break;
-			case 4: // lu
-				result = ((acc >> 4) & 0xF) | data << 4;
-				carryRes = acc != data;
-				break;
-			case 5: // xor
-				result = acc ^ data;
-				carryRes = acc == data;
-				break;
-			case 6: // and
-				result = acc & data;
-				carryRes = acc < data;
-				break;
-			case 7: // or
-				result = acc | data;
-				carryRes = acc >= data;
-				break;
-			case 8: // sl
-				carryRes = (acc >> 7) & 1; 
-				result = acc << 1;
-				break;
-			case 9: // slc
-				carryRes = (acc >> 7) & 1;
-				result = (acc << 1) | carry;
-				break;
-			case 0xA: // slr
-				carryRes = (acc >> 7) & 1;
-				result = (acc << 1) | carryRes;
-				break;
-			case 0xB: // sla
-				carryRes = (acc >> 7) & 1;
-				result = (acc << 1) | (acc & 1);
-				break;
-			case 0xC: // sr
-				carryRes = acc & 1;
-				result = (acc >> 1) & 0b01111111;
-				break;
-			case 0xD: // src
-				carryRes = acc & 1;
-				result = ((acc >> 1) & 0b01111111) | (((int)carry) << 7);
-				break;
-			case 0xE: // srr
-				carryRes = acc & 1;
-				result = ((acc >> 1) & 0b01111111) | (((int)carryRes) << 7);
-				break;
-			case 0xF: // sra
-				carryRes = acc & 1;
-				result = acc >> 1;
-				break;
-		}
-		if(op & 0b00100000) {
-			carry = carryRes;
-		}
-		if(op & 0b00010000) {
-			acc = result;
 		}
 	}
 	return true;
