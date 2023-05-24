0fcd6fa0b818f875d19b6671caff4f79920e8c1d
LithosphereRocketry <greatmaharg@gmail.com>
2023-05-24 03:59:37 -0400

    Program functions

--- Toolchain/include/programs.h
+++ Toolchain/include/programs.h
@@ -8,6 +8,10 @@ typedef struct program {
     instruction_t* end;
 } program_t;
 
+const extern program_t PROGRAM_INITIALIZER;
 
+void append(program_t* program, instruction_t* instruction);
+void print(program_t* program);
+void delete(program_t* program);
 
 #endif
--- Toolchain/src/programs.c
+++ Toolchain/src/programs.c
@@ -1,2 +1,29 @@
 #include "programs.h"
 
+const program_t PROGRAM_INITIALIZER = {NULL, NULL};
+
+void append(program_t* program, instruction_t* instruction) {
+    if(program->end) {
+        insertInstruction(program->end, instruction);
+    } else {
+        program->start = instruction;
+    }
+    program->end = instruction;
+}
+
+void print(program_t* program) {
+    instruction_t* instr = program->start;
+    while(instr) {
+        printInstruction(instr);
+        instr = instr->next;
+    }
+}
+
+void delete(program_t* program) {
+    instruction_t* instr = program->start;
+    while(instr) {
+        instruction_t* nextinst = instr->next;
+        deleteInstruction(instr);
+        instr = nextinst;
+    }
+}
