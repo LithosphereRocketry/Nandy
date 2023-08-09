55f35039d0248d602b02d2e5622a0b98e0571f9e
ben <greatmaharg@gmail.com>
2023-08-09 19:19:51 -0400

    Some basic assertions

---
+++ Simulation/src/debug.v
@@ -0,0 +1,9 @@
+module assert #(parameter message = "unspecified")
+               (input value);
+    task test ();
+        if (~value) begin
+            $display("Assertion failed: %s", message);
+            $finish(1);
+        end
+    endtask
+endmodule
--- Simulation/tests/tb_nand00.v
+++ Simulation/tests/tb_nand00.v
@@ -1,8 +1,6 @@
 `timescale 1ns/1ps
 
 module tb_nand00();
-    `include "debug.vh"
-
     reg a, b;
     wire q;
 
@@ -12,27 +10,39 @@ module tb_nand00();
         .q(q)
     );
 
+    assert #("Output 0 when 1 expected") high (.value(q));
+    assert #("Output 1 when 0 expected") low (.value(~q));
+    assert #("Ouptut defined when undefined expected") undf (.value(q == 1'bx));
+
     initial begin
+        undf.test();
         a = 1'b0;
         b = 1'b0;
-        
-        assert #(message = "Heehee hoohoo") (1'b0);
-
         #50;
+        high.test();
         a = 1'b1;
         #50;
+        high.test();
         b = 1'b1;
         #50;
+        low.test();
         a = 1'b0;
         #50;
+        high.test();
         b = 1'b0;
         #50;
+        high.test();
         a = 1'bx;
         #50;
+        high.test();
         b = 1'bx;
         #50;
+        undf.test();
         a = 1'b1;
         #50;
+        high.test();
+        $display("All tests passed");
+        $finish(0);
     end
 
 endmodule
