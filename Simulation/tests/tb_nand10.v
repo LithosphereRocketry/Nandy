cd9d288527d6b616ad5531e11737a4641ea04897
ben <greatmaharg@gmail.com>
2023-11-19 17:32:19 -0500

    Updated nand10 testbench

--- Simulation/tests/tb_nand10.v
+++ Simulation/tests/tb_nand10.v
@@ -1,29 +1,20 @@
 `timescale 1ns/1ps
 
 module tb_nand10();
-    reg [2:0] inp;
-    wire q;
+    wire a, b, c, q;
+    wire realq;
+    assign realq = ~(a & b & c);
 
     nand10 testGate(
-        .a(inp[0]),
-        .b(inp[1]),
-        .c(inp[2]),
+        .a(a),
+        .b(b),
+        .c(c),
         .q(q)
     );
-
-    assert #("Incorrect value!") val (.value(q == ~(inp[0] & inp[1] & inp[2])));
-
-    initial begin
-        inp = 0;
-        #50;
-        repeat (8) begin
-            $display("Testing value %b", inp);
-            inp = inp + 1;
-            #50;
-            val.test();
-        end
-        $display("All tests passed");
-        $finish(0);
-    end
-
+    
+    combitest #("nand10", 3, 1, 20) tester(
+        .comp_in({a, b, c}),
+        .verify(realq),
+        .comp_out(q)
+    );
 endmodule
