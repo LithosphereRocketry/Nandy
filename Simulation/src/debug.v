fb901f692462c3a31f3c77bfaac0686c57f66a79
ben <greatmaharg@gmail.com>
2023-11-19 17:21:44 -0500

    Combinational logic testing tools

--- Simulation/src/debug.v
+++ Simulation/src/debug.v
@@ -1,3 +1,5 @@
+`timescale 1ns/1ps
+
 module assert #(parameter message = "unspecified")
                (input value);
     task test;
@@ -6,4 +8,26 @@ module assert #(parameter message = "unspecified")
             $finish(1);
         end
     endtask
+endmodule
+
+module combitest #(parameter name = "unnamed", parameter inbits = 1,
+                   parameter outbits = 1, parameter delay = 10)
+                  (output reg [(inbits-1):0] comp_in,
+                   input wire [(outbits-1):0] verify,
+                   input wire [(outbits-1):0] comp_out);
+    
+
+    initial begin
+        comp_in = 0;
+        repeat (1 << inbits) begin
+            comp_in ++;
+            #(delay);
+            if (comp_out !== verify) begin
+                $display("Test %s failed: input %b -> expected %b, got %b",
+                    name, comp_in, verify, comp_out);
+                $finish(1);
+            end
+        end
+        $finish(0);
+    end
 endmodule
--- Simulation/tests/tb_nand00.v
+++ Simulation/tests/tb_nand00.v
@@ -1,8 +1,9 @@
 `timescale 1ns/1ps
 
 module tb_nand00();
-    reg a, b;
-    wire q;
+    wire a, b, q;
+    wire realq;
+    assign realq = ~(a & b);
 
     nand00 testGate(
         .a(a),
@@ -10,39 +11,10 @@ module tb_nand00();
         .q(q)
     );
 
-    assert #("Output 0 when 1 expected") high (.value(q));
-    assert #("Output 1 when 0 expected") low (.value(~q));
-    assert #("Ouptut defined when undefined expected") undf (.value(q == 1'bx));
-
-    initial begin
-        undf.test();
-        a = 1'b0;
-        b = 1'b0;
-        #50;
-        high.test();
-        a = 1'b1;
-        #50;
-        high.test();
-        b = 1'b1;
-        #50;
-        low.test();
-        a = 1'b0;
-        #50;
-        high.test();
-        b = 1'b0;
-        #50;
-        high.test();
-        a = 1'bx;
-        #50;
-        high.test();
-        b = 1'bx;
-        #50;
-        undf.test();
-        a = 1'b1;
-        #50;
-        high.test();
-        $display("All tests passed");
-        $finish(0);
-    end
+    combitest #("nand00", 2, 1, 20) tester(
+        .comp_in({a, b}),
+        .verify(realq),
+        .comp_out(q)
+    );
 
 endmodule
