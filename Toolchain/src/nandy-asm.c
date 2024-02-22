874cfe16ef14c7325937190a509cb050b3d2579e
ben <greatmaharg@gmail.com>
2024-02-21 23:23:34 -0500

    datastructures for new assembler

--- Toolchain/src/nandy-asm.c
+++ Toolchain/src/nandy-asm.c
@@ -2,6 +2,7 @@
 #include <stdlib.h>
 
 #include "iotools.h"
+#include "nandy_tools.h"
 
 int main(int argc, char** argv) {
     if(argc != 2) {
@@ -18,3 +19,25 @@ int main(int argc, char** argv) {
     }
     free(text);
 }
+
+typedef struct unresolved {
+    addr_t location;
+    char* str;
+} unresolved_t;
+
+typedef struct label {
+    char* name;
+    int64_t value;
+} label_t;
+
+typedef struct asmstate {
+    cpu_state_t cpu;
+    
+    size_t resolved_sz;
+    size_t resolved_cap;
+    label_t* resolved;
+
+    size_t unresolved_sz;
+    size_t unresolved_cap;
+    unresolved_t unresolved;
+} asmstate_t;
