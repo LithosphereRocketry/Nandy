517097e264135b69d478b3913db0df9edc605ff0
bry <greatmaharg@gmail.com>
2024-05-16 23:30:50 -0400

    Fixed a major bug in register jump instructions; also multiply routines pog

--- Toolchain/src/instrs_temp_macros.c
+++ Toolchain/src/instrs_temp_macros.c
@@ -13,6 +13,7 @@ static bool resolveRda(asm_state_t* state, const char* text, addr_t pos, FILE* d
         }
         state->rom[pos+5] = (value >> 8) & 0xFF;
         state->rom[pos+2] = value & 0xFF;
+        printf("%lx %hhx %hhx\n", value, state->rom[pos+5], state->rom[pos+2]);
         return true;
     } else {
         if(debug) fprintf(debug, "Parse failed: %i\n", status);
