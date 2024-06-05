7ac4418ec593de95d0147fcb98855bfa2df3c292
bry <greatmaharg@gmail.com>
2024-06-04 21:31:46 -0600

    Basic Lisp AST parsing

---
+++ ReferenceImplementations/src/test_lisp.c
@@ -0,0 +1,149 @@
+/**
+ * Proof-of-concept basic LISP implementation for extremely limited systems
+*/
+
+#include <stdbool.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include <ctype.h>
+
+typedef enum {
+    TYPE_UNRESOLVED,
+    TYPE_NULL,
+    TYPE_INT,
+    TYPE_PAIR
+} lisp_type_t;
+
+typedef struct {
+    lisp_type_t type;
+    bool dynamic;
+    void* data;
+} lisp_obj_t;
+
+typedef struct {
+    lisp_obj_t car;
+    lisp_obj_t cdr;
+} lisp_pair_t;
+
+#define STACK_SZ (1<<8)
+lisp_obj_t stack[STACK_SZ];
+size_t sp = STACK_SZ;
+
+void error(const char* msg) {
+    printf("Error! %s\nExiting\n", msg);
+    exit(-1);
+}
+
+void push_obj(lisp_obj_t obj) {
+    if(sp == 0) { error("Out of stack space"); }
+    stack[--sp] = obj;
+}
+lisp_obj_t pop_obj() {
+    if(sp == STACK_SZ) { error("Stack underflow"); }
+    return stack[sp++];
+}
+
+long int parseInt(FILE* infile) {
+    long int acc = 0;
+    int chr;
+    while(isdigit((chr = getc(infile)))) {
+        acc *= 10;
+        acc += chr - '0';
+    }
+    ungetc(chr, infile);
+    return acc;
+}
+
+lisp_obj_t parseInput(FILE* infile) {
+    while(1) {
+        int next = getc(infile);
+        if(isspace(next)) {
+            // ignore
+        } else if(next == '(') {
+            lisp_obj_t newobj = {TYPE_UNRESOLVED, false, NULL};
+            push_obj(newobj);
+        } else if(next == ')') {
+            lisp_obj_t tail = {TYPE_NULL, false, NULL};
+            while(1) {
+                lisp_obj_t nextobj = pop_obj();
+                if(nextobj.type == TYPE_UNRESOLVED) {
+                    push_obj(tail);
+                    break;
+                } else {
+                    lisp_pair_t* newpair = malloc(sizeof(lisp_pair_t));
+                    newpair->car = nextobj;
+                    newpair->cdr = tail;
+                    lisp_obj_t newtail = {TYPE_PAIR, true, newpair};
+                    tail = newtail;
+                }
+            }
+        } else if(isdigit(next)) {
+            long int* i = malloc(sizeof(long int));
+            ungetc(next, infile);
+            *i = parseInt(infile);
+            lisp_obj_t newobj = {TYPE_INT, true, i};
+            push_obj(newobj);
+        } else {
+            error("Unrecognized symbol");
+        }
+
+        // If only one thing is left on the stack, and it's not an open-paren
+        // placeholder, we've reached the end of the current statement and we
+        // can spit out an answer
+        if(sp == STACK_SZ-1 && stack[sp].type != TYPE_UNRESOLVED) {
+            return pop_obj();
+        }
+    }
+}
+
+void free_obj(lisp_obj_t obj) {
+    if(obj.dynamic) {
+        if(obj.type == TYPE_PAIR) {
+            lisp_pair_t* pair = obj.data;
+            free_obj(pair->car);
+            free_obj(pair->cdr);
+        }
+        free(obj.data);
+    }
+}
+
+void print_obj(lisp_obj_t obj) {
+    switch(obj.type) {
+        case TYPE_UNRESOLVED: printf("[UNRESOLVED]"); break;
+        case TYPE_INT: printf("%li", *(long int*) obj.data); break;
+        case TYPE_NULL: printf("()"); break;
+        case TYPE_PAIR: {}
+            printf("(");
+            for(lisp_pair_t* pair = obj.data; pair; ) {
+                print_obj(pair->car);
+                switch(pair->cdr.type) {
+                    case TYPE_PAIR:
+                        printf(" ");
+                        pair = pair->cdr.data;
+                        break;
+                    case TYPE_NULL:
+                        printf(")");
+                        pair = NULL;
+                        break;
+                    default:
+                        printf(" . ");
+                        print_obj(pair->cdr);
+                        printf(")");
+                        pair = NULL;
+                        break;
+                }
+            }
+            break;
+    }
+}
+
+int main(int argc, char** argv) {
+    while(1) {
+        printf("> ");
+        lisp_obj_t obj = parseInput(stdin);
+        print_obj(obj);
+        free_obj(obj);
+        printf("\n");
+        exit(0);
+    }
+}
