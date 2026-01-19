a8eaa9e9b731aec49df74bebe23fdebc1aa78bd8
bry <greatmaharg@gmail.com>
2026-01-18 20:37:29 -0700

    Merged stash changes to compactflash emulation

--- Toolchain/src/ios_compactflash.c
+++ Toolchain/src/ios_compactflash.c
@@ -1,6 +1,8 @@
 #include "nandy_ios.h"
 
 #include <stdio.h>
+#include <string.h>
+
 #include <fcntl.h>
 #include <termios.h>
 #include <unistd.h>
@@ -19,6 +21,10 @@ enum cf_registers {
     CF_LBA_23_16 = 5,
     CF_LBA_27_24_MODE = 6,
     CF_STATUS_CMD = 7
+};
+
+enum cf_status {
+    CF_DRQ = 0b00001000
 };
 
 static bool first_access = true;
@@ -42,9 +48,65 @@ static uint8_t error_register;
 
 static bool feat_8bit;
 
+static char* seek_ptr = NULL;
+static size_t region_size = 0;
+
 static void cf_abort() {
     registers[CF_STATUS_CMD] |= 0b1;
     error_register |= 0b101;
+}
+
+// place a 16 bit value in correct endianness into two 8-bit entries
+// I can't find super clear documentation on what endianness CF IDENTIFY DRIVE
+// uses so I'm doing it this way so it can be easily changed
+static void put16(char* buf, uint16_t val) {
+    // I *think* LE is correct
+    buf[0] = val & 0xFF;
+    buf[1] = val >> 8;
+}
+
+static char ident_buffer[512];
+static void set_up_ident() {
+    size_t n_sectors = disksize/512;
+    if(n_sectors > 0xFFFFFFFF) {
+        fprintf(stderr, "Warning: disk size of %zu sectors does not fit in 32-"
+                "bit disk size parameter, size may be truncated", n_sectors);
+    }
+
+    memset(ident_buffer, 0, 512); // probably unnecessary but it doesn't hurt
+    put16(ident_buffer + 2*0, 0x848A); // CF removable disk signature
+    // TODO heads
+    put16(ident_buffer + 2*5, 512); // bytes/sector
+    // TODO sectors/track
+    // TODO sectors/card
+    memcpy(ident_buffer + 2*10, "SERIAL_NUMBER_123456", 20);
+    put16(ident_buffer + 2*20, 2); // buffer type
+    put16(ident_buffer + 2*21, 2); // buffer size
+    put16(ident_buffer + 2*22, 4); // ECC bytes on R/W Long
+    memcpy(ident_buffer + 2*23, "FW111111", 8);
+    memcpy(ident_buffer + 2*27, "MODEL_NUMBER_1234567890_ABCDEFG_0000000", 40);
+    put16(ident_buffer + 2*47, 1); // max 1 sector on R/W multiple
+    put16(ident_buffer + 2*49, 0x0200); // LBA supported
+    put16(ident_buffer + 2*51, 0x0200); // PIO mode 2
+    put16(ident_buffer + 2*53, 3); // field validity (?)
+    // TODO current cylinders
+    // TODO current heads
+    // TODO current sectors
+    put16(ident_buffer + 2*57, n_sectors & 0xFFFF); // current LBA capacity
+    put16(ident_buffer + 2*58, n_sectors >> 16);
+    put16(ident_buffer + 2*59, 0x0100); // multiple sector setting is valid (?)
+    put16(ident_buffer + 2*57, n_sectors & 0xFFFF); // total number of LBA sect.
+    put16(ident_buffer + 2*58, n_sectors >> 16); // is this ever different?
+    // I don't use PIO so these numbers are pretty irrelevant
+    put16(ident_buffer + 2*64, 3); // advanced PIO supported (lies)
+    put16(ident_buffer + 2*64, 78); // PIO cycle time w/o flow control
+    put16(ident_buffer + 2*64, 78); // PIO cycle time w/ flow control
+}
+
+static void seek_to(char* buf, size_t size) {
+    seek_ptr = buf;
+    region_size = size;
+    if(size) registers[CF_STATUS_CMD] |= CF_DRQ;
 }
 
 static void cf_command(uint8_t cmd) {
@@ -63,6 +125,10 @@ static void cf_command(uint8_t cmd) {
                         registers[CF_ERROR_FEATURE]);
                     cf_abort();
             }
+            break;
+        case 0xEC: // identify drive
+            seek_to(ident_buffer, 512);
+            break;
         default:
             fprintf(stderr, "Received an unknown CF command 0x%hhx", cmd);
                     cf_abort();
@@ -107,6 +173,7 @@ bool io_step_compactflash(cpu_state_t* cpu, bool active) {
                 fprintf(stderr, "Warning, could not open file '%s', defaulting to no disk", arg_diskimg.result.value);
             }
         }
+        set_up_ident();
         first_access = false;
     }
 
@@ -127,13 +194,26 @@ bool io_step_compactflash(cpu_state_t* cpu, bool active) {
                 }
             } else {
                 switch(reg_sel) {
-                    case CF_ERROR_FEATURE:
-                        cpu->acc = error_register;
-                        break;
-                    default:
-                        cpu->acc = registers[reg_sel];
-                }
-            }            
+                case CF_ERROR_FEATURE:
+                    cpu->acc = error_register;
+                    break;
+                case CF_DATA:
+                    if(seek_ptr) {
+                        cpu->acc = *seek_ptr;
+                        if(cpu->io_rd) {
+                            seek_ptr ++;
+                            region_size --;
+                            if(!region_size) {
+                                registers[CF_STATUS_CMD] &= ~CF_DRQ;
+                                seek_ptr = NULL;
+                            }
+                        }
+                    }
+                    break;
+                default:
+                    cpu->acc = registers[reg_sel];
+                }            
+            }
         }
 
         if(cpu->io_wr) {
