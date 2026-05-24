e5433d9c006e493ac867ab895ebef95326a3c010
Bry <greatmaharg@gmail.com>
2026-05-24 00:23:35 -0600

    Mock memory module

---
+++ Simulation/src/memory.v
@@ -0,0 +1,20 @@
+module memory(
+        input clk,
+
+        input [15:0] addr,
+        input [7:0] din,
+        input wr,
+
+        output [7:0] dout
+    );
+
+    reg [7:0] rom [0:32767];
+    reg [7:0] ram [0:32767];
+
+    assign dout = wr ? 8'hxx :
+                  addr[15] ? ram[addr[14:0]] :
+                  rom[addr[14:0]];
+
+    always @(posedge clk) if(wr) ram[addr[14:0]] <= din;
+
+endmodule
